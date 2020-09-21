8a1d20b67919e751198c24ff7bff6e8108da8fe5
Christopher Barrett <municipalshock@gmail.com>
2020-09-21 11:23:00 -0700

    initial commit

--- sleep-tracker/src/App.js
+++ sleep-tracker/src/App.js
@@ -1,39 +1,41 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
-
-
-
-
-
   return (
     <div className="App">
+      <h2>Sleep better. Feel better. Live better.</h2>
+      <div className="logInForm">
+        <h2>Log In</h2>
+        <p>Tracker for better sleep.</p>
+        <br />
 
-        {/* <div className="headerDiv"> */}
-            <header>
-                <h1>.logo</h1>
-            </header>
-        {/* </div> */}
-       
-        <div className="logInForm">
-            <h2>Log In</h2>
-            <p>Tracker a better sleep.</p>
-            <br/>
-
-               <input className='email' name='email' type='email' placeholder='Enter your Email'/><br/>
-               <br/>
-               <input className='password' name='password' type='password' placeholder='Enter your Password'/><br/>
-               <br/>
-               <button>Log In</button>
-               <br/>
-               <br/>
-               <a href="url">Not a Member?<br/>
-               Sign-Up to get better sleep.
-               </a>
-        </div>
-
+        <input
+          className="email"
+          name="email"
+          type="email"
+          placeholder="Enter your Email"
+        />
+        <br />
+        <br />
+        <input
+          className="password"
+          name="password"
+          type="password"
+          placeholder="Enter your Password"
+        />
+        <br />
+        <br />
+        <button>Log In</button>
+        <br />
+        <br />
+        <a href="url">
+          Not a Member?
+          <br />
+          Sign-Up to get better sleep.
+        </a>
+      </div>
     </div>
   );
 }
